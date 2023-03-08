f24e01293f50772b76129ada9b0ebfaa5e161fd1
hoarguo <hoarguo@gmail.com>
2023-03-08 14:57:46 +0800

    third

---
+++ sub1.py
@@ -0,0 +1,15 @@
+import redis
+
+r = redis.Redis(host='192.168.16.66', port=6379, db=1)
+r.publish('my_redis_channel','aaa')
+
+sub = r.pubsub()
+sub.subscribe('my_redis_channel')
+for message in sub.listen():
+    print('Got message', message)
+    print(type(message))
+    # if (
+    #     isinstance(message.get('data'), bytes) and
+    #     message['data'].decode() == 'GREETING'
+    # ):
+        # print('Hello')
